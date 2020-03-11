9aa310b2e3dc420a8332c733081f35d656df7f05
louise1991 <lelivrelouise@gmail.com>
2020-03-11 21:52:21 +0100

    Update clock counter.

--- i2c-master.vhd
+++ i2c-master.vhd
@@ -45,7 +45,7 @@ ARCHITECTURE behavioral OF i2c_master IS
 
     --Counters signals--
     SIGNAL r_clock_counter : INTEGER RANGE 0 TO CLK_DIV * 4; -- counter to divide clock
-    SIGNAL r_bit_loop_counter : INTEGER RANGE 0 TO 7; -- bit counter to iterate on a byte
+    SIGNAL r_bit_loop_counter : INTEGER RANGE 7 DOWNTO 0; -- bit counter to iterate on a byte
     SIGNAL r_write_counter : INTEGER RANGE 0 TO 31; -- number of word (bytes) to be written
     SIGNAL r_read_counter : INTEGER RANGE 0 TO 31; -- number of word (bytes) to be read
     SIGNAL r_timeout_counter : std_logic_vector(15 DOWNTO 0); --timeout counter in case of no received words while waiting one
@@ -57,6 +57,7 @@ ARCHITECTURE behavioral OF i2c_master IS
     --Data signals--
     SIGNAL r_data_in : std_logic_vector(7 DOWNTO 0); -- register to collect new data to be written
     SIGNAL r_data_out : std_logic_vector(7 DOWNTO 0);
+    SIGNAL w_data_out : std_logic;
     SIGNAL r_data_ready : std_logic;
     SIGNAL r_data_access : std_logic;
     SIGNAL r_divided_clock : std_logic;
@@ -67,7 +68,10 @@ ARCHITECTURE behavioral OF i2c_master IS
 BEGIN
     o_busy <= r_busy;
     w_i2c_start <= i_i2c_start;
+    --condition à mettre sur ligne SDA pour état de repos?
+    o_sda <= w_data_out;
     o_scl <= r_divided_clock;
+
     -- counters
 
     p_read_counter : PROCESS (i_clk, i_rst)
@@ -121,73 +125,81 @@ BEGIN
                     r_clock_counter := r_clock_counter + 1;
                 END IF;
             ELSIF (r_clock_active = '0') THEN
-                r_clock_counter = 0;
+                r_clock_counter := 0;
                 r_divided_clock = '1';
             END IF;
         END IF;
     END PROCESS p_clock_counter;
 
-    p_bit_loop : PROCESS (i_clk, i_rst)
-    BEGIN
-        IF i_rst = '0' THEN
-
-        ELSIF rising_edge(clk) THEN
-
-        END IF;
-    END PROCESS p_bit_loop;
-
-    -- State changer and FSM
-    p_state : PROCESS (i_clk, i_rst)
+    p_bit_loop_counter : PROCESS (i_clk, i_rst)
     BEGIN
         IF (i_rst = '0') THEN
-            r_st_present <= ST_RESET;
-        ELSIF (rising_edge(i_clk)) THEN
-            r_st_present <= w_st_next;
-        END IF;
-    END PROCESS p_state;
-
-    p_fsm : PROCESS (r_st_present)
-    BEGIN
-        CASE r_st_present IS
-            WHEN ST_IDLE =>
-                r_busy <= '0';
-                r_error <= '0';
-                IF (w_i2c_start = '1') THEN
-                    w_st_next <= ST_START;
-                ELSE
-                    w_st_next <= r_st_present;
+            r_bit_loop_counter := 7;
+        ELSIF rising_edge(clk) THEN
+            IF (r_clock_active = '1') THEN
+                IF (r_clock_counter = 0) THEN
+                    -- on présente à la sortie la valeur du bit de data
+                    w_data_out <= r_data_out(r_bit_loop_counter);
+                ELSIF (r_clock_counter = CLK_DIV) THEN
+                    r_bit_loop_counter := r_bit_loop_counter - 1;
+                ELSIF (r_clock_counter = CLK_DIV & & r_bit_loop_counter = 0) THEN
+                    r_bit_loop_counter := 7;
                 END IF;
-            WHEN ST_START =>
-                r_busy <= '1';
-                r_error <= '0';
-            WHEN ST_ADDRESSING =>
-                r_busy <= '1';
-                r_error <= '0';
-            WHEN ST_ACKNOWLEDGMENT =>
-                r_busy <= '1';
-                r_error <= '0';
-            WHEN ST_COUNTERS_STATUS =>
-                r_busy <= '1';
-                r_error <= '0';
-            WHEN ST_WRITE_DATA =>
-                r_busy <= '1';
-                r_error <= '0';
-            WHEN ST_READ_DATA =>
-                r_busy <= '1';
-                r_error <= '0';
+            END IF;
+        END PROCESS p_bit_loop_counter;
 
-            WHEN ST_ERROR =>
-                r_busy <= '1';
-                r_error <= '1';
+        -- State changer and FSM
+        p_state : PROCESS (i_clk, i_rst)
+        BEGIN
+            IF (i_rst = '0') THEN
+                r_st_present <= ST_RESET;
+            ELSIF (rising_edge(i_clk)) THEN
+                r_st_present <= w_st_next;
+            END IF;
+        END PROCESS p_state;
 
-            WHEN ST_END =>
-                r_busy <= '1';
-                r_error <= '0';
-            WHEN OTHERS =>
-                r_busy <= '0';
-                r_error <= '0';
-                w_st_next <= r_st_present;
-        END CASE;
-    END PROCESS p_fsm;
+        p_fsm : PROCESS (r_st_present)
+        BEGIN
+            CASE r_st_present IS
+                WHEN ST_IDLE =>
+                    r_busy <= '0';
+                    r_error <= '0';
+                    IF (w_i2c_start = '1') THEN
+                        w_st_next <= ST_START;
+                    ELSE
+                        w_st_next <= r_st_present;
+                    END IF;
+                WHEN ST_START =>
+                    r_busy <= '1';
+                    r_error <= '0';
+                WHEN ST_ADDRESSING =>
+                    r_busy <= '1';
+                    r_error <= '0';
+                WHEN ST_ACKNOWLEDGMENT =>
+                    r_busy <= '1';
+                    r_error <= '0';
+                WHEN ST_COUNTERS_STATUS =>
+                    r_busy <= '1';
+                    r_error <= '0';
+                WHEN ST_WRITE_DATA =>
+                    r_busy <= '1';
+                    r_error <= '0';
+                WHEN ST_READ_DATA =>
+                    r_busy <= '1';
+                    r_error <= '0';
 
-END behavioral;
+                WHEN ST_ERROR =>
+                    r_busy <= '1';
+                    r_error <= '1';
+
+                WHEN ST_END =>
+                    r_busy <= '1';
+                    r_error <= '0';
+                WHEN OTHERS =>
+                    r_busy <= '0';
+                    r_error <= '0';
+                    w_st_next <= r_st_present;
+            END CASE;
+        END PROCESS p_fsm;
+
+    END behavioral;
