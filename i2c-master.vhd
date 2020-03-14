876d0286af369e2c48fb98fd7f8c12a1529cd359
louise1991 <lelivrelouise@gmail.com>
2020-03-14 17:01:00 +0100

    Finish i2c_master.

--- i2c-master.vhd
+++ i2c-master.vhd
@@ -21,7 +21,6 @@ ENTITY i2c_master IS
         o_data : OUT std_logic_vector(7 DOWNTO 0); -- data out from i2c to assess when data_ready is high
         o_busy : OUT std_logic; -- busy signal when i2c master interface is out of idle state
         o_error : OUT std_logic; -- error signal to evaluate error code
-        o_error_code : OUT std_logic_vector(3 DOWNTO 0); -- error code to detect abnormal behavior
         o_data_access : OUT std_logic; -- data access signal to evaluate i_data
         o_data_ready : OUT std_logic; -- data ready to signal when o_data can be evaluated
         o_i2c_end : OUT std_logic; -- end of transmission signal
@@ -47,7 +46,6 @@ ARCHITECTURE behavioral OF i2c_master IS
     SIGNAL r_bit_loop_counter : INTEGER RANGE 7 DOWNTO 0; -- bit counter to iterate on a byte
     SIGNAL r_write_counter : INTEGER RANGE 0 TO 31; -- number of word (bytes) to be written
     SIGNAL r_read_counter : INTEGER RANGE 0 TO 31; -- number of word (bytes) to be read
-    SIGNAL r_timeout_counter : std_logic_vector(15 DOWNTO 0); --timeout counter in case of no received words while waiting one
     --Status signals--
     SIGNAL r_busy : std_logic; -- busy status
     SIGNAL r_read_mode : std_logic;
@@ -56,10 +54,10 @@ ARCHITECTURE behavioral OF i2c_master IS
     SIGNAL r_error : std_logic; -- error status
     SIGNAL r_error_code : std_logic_vector(2 DOWNTO 0);
     SIGNAL w_i2c_start : std_logic;
-    SIGNAL w_i2c_end : std_logic;
+    SIGNAL r_i2c_end : std_logic;
     SIGNAL r_ack : std_logic;
     --Data signals--
-    SIGNAL r_data : std_logic_vector(7 DOWNTO 0); -- 
+    SIGNAL r_data : std_logic_vector(7 DOWNTO 0);
     SIGNAL r_data_ready : std_logic;
     SIGNAL r_data_access : std_logic;
     SIGNAL r_divided_clock : std_logic;
@@ -68,44 +66,16 @@ ARCHITECTURE behavioral OF i2c_master IS
     SIGNAL w_st_next : t_i2c_master_fsm; -- next state
 
 BEGIN
-    o_busy <= r_busy;
     w_i2c_start <= i_i2c_start;
-    o_i2c_end <= w_i2c_end;
+    o_busy <= r_busy;
+    o_i2c_end <= r_i2c_end;
     o_scl <= r_divided_clock;
+    o_error <= r_error;
+    o_data_access <= r_data_access;
+    o_data_ready <= r_data_ready;
 
     -- counters
-
-    p_read_counter : PROCESS (i_clk, i_rst)
-    BEGIN
-        IF (i_rst = '1') THEN
-            r_read_counter := 0;
-        ELSIF (w_i2c_start = '1') THEN
-            r_read_counter <= i_read_byte_nb;
-        ELSIF --(condition de décrément à voir en fin d'étape READ) THEN
-            r_read_counter <= r_read_counter - 1;
-            o_data <= r_data_out;
-            --penser a enchainer le r_data_ready
-        ELSE
-            r_read_counter <= r_read_counter;
-        END IF;
-    END PROCESS p_read_counter;
-
-    p_write_counter : PROCESS (i_clk, i_rst)
-    BEGIN
-        IF (i_rst = '1') THEN
-            r_write_counter := 0;
-        ELSIF (w_i2c_start = '1') THEN
-            r_write_counter <= i_write_byte_nb;
-        ELSIF --(condition de décrément à voir en fin d'étape WRITE) THEN
-            r_write_counter <= r_write_counter - 1;
-            r_data_in <= i_data;
-            --penser à enchainer le r_data_access
-        ELSE
-            r_write_counter <= r_write_counter;
-        END IF;
-    END PROCESS p_write_counter;
-
-    p_clock_counter : PROCESS (i_clk, i_rst)
+    p_clock_counter : PROCESS (i_clk, i_rst, r_clock_active)
     BEGIN
         IF (i_rst = '0') THEN
             r_clock_counter = 0;
@@ -132,10 +102,11 @@ BEGIN
         END IF;
     END PROCESS p_clock_counter;
 
-    p_bit_loop_counter : PROCESS (i_clk, i_rst)
+    p_bit_loop_counter : PROCESS (i_clk, i_rst, r_clock_active, r_clock_counter, r_write_mode, r_read_mode, r_read_write_done)
     BEGIN
         IF (i_rst = '0') THEN
             r_bit_loop_counter := 7;
+            o_sda <= '1';
         ELSIF rising_edge(clk) THEN
             IF ((r_clock_active = '1') AND (r_read_write_done = '0')) THEN
                 IF (r_clock_counter = 0) THEN
@@ -146,21 +117,14 @@ BEGIN
                     END IF;
                 ELSIF (r_clock_counter = CLK_DIV * 2) THEN
                     IF (r_read_mode = '1') THEN
-                        r_data <= i_sda;
+                        r_data(r_bit_loop_counter) <= i_sda;
                     ELSIF (r_read_mode = '0')
                         r_data <= r_data;
                     END IF;
                 ELSIF (r_clock_counter = CLK_DIV * 4) THEN
-                    IF (r_bit_loop_counter = '0') THEN
+                    IF (r_bit_loop_counter = 0) THEN
                         r_bit_loop_counter := 7;
                         r_read_write_done <= '1';
-                        IF (r_write_mode = '1') THEN
-                            r_write_counter <= r_write_counter - 1;
-                            r_data_access <= '1';
-                        ELSIF (r_read_mode = '1') THEN
-                            r_read_counter <= r_read_counter - 1;
-                            r_data_ready <= '1';
-                        END IF;
                     ELSE
                         r_bit_loop_counter := r_bit_loop_counter - 1;
                     END IF;
@@ -169,7 +133,51 @@ BEGIN
         END IF;
     END PROCESS p_bit_loop_counter;
 
-    p_acknowledgement : PROCESS (i_clk, i_rst)
+    p_addressing_start : PROCESS (i_clk, i_rst, w_i2c_start)
+    BEGIN
+        IF (i_rst = '0') THEN
+            r_data <= (OTHERS => '0');
+        ELSIF rising_edge(clk) THEN
+            IF (w_i2c_start = '1') THEN
+                r_data <= std_logic_vector(std_logic_vector(resize(unsigned(i_device_address), r_data'length)) SLL 1) & i_R_W;
+                r_write_counter := r_write_counter + 1;
+            ELSE
+                r_data <= r_data;
+            END IF;
+        END IF;
+    END PROCESS p_addressing_routing;
+
+    p_read_counter : PROCESS (i_clk, i_rst, r_read_mode, r_bit_loop_counter)
+    BEGIN
+        IF (i_rst = '1') THEN
+            r_read_counter := 0;
+        ELSIF (w_i2c_start = '1') THEN
+            r_read_counter := i_read_byte_nb;
+        ELSIF (r_read_mode = '1' AND r_bit_loop_counter = 0) THEN
+            r_read_counter := r_read_counter - 1;
+            r_data_out <= r_data;
+            r_data_ready <= '1';
+        ELSE
+            r_read_counter := r_read_counter;
+        END IF;
+    END PROCESS p_read_counter;
+
+    p_write_counter : PROCESS (i_clk, i_rst, r_write_mode, bit_loop_counter)
+    BEGIN
+        IF (i_rst = '1') THEN
+            r_write_counter := 0;
+        ELSIF (w_i2c_start = '1') THEN
+            r_write_counter := i_write_byte_nb;
+        ELSIF (r_write_mode = '1' AND r_bit_loop_counter = 0) THEN
+            r_write_counter := r_write_counter - 1;
+            r_data <= i_data;
+            r_data_access <= '1';
+        ELSE
+            r_write_counter := r_write_counter;
+        END IF;
+    END PROCESS p_write_counter;
+
+    p_acknowledgement : PROCESS (i_clk, i_rst, r_clock_counter, r_read_write_done)
     BEGIN
         IF (i_rst = '0') THEN
             r_ack <= '0';
@@ -177,28 +185,12 @@ BEGIN
             IF (r_read_write_done = '1' AND r_clock_counter = CLK_DIV * 2) THEN
                 r_ack <= NOT(i_sda);
             ELSIF (r_clock_counter = CLK_DIV * 4) THEN
-                r_read_write_done = '0';
+                r_read_write_done <= '0';
             END IF;
         END IF;
     END PROCESS p_acknowledgement;
-
-    p_addressing_start : PROCESS (i_clk, i_rst)
-    BEGIN
-        IF (i_rst = '0') THEN
-            r_data <= (OTHERS => '0');
-        ELSIF rising_edge(clk) THEN
-            IF (w_i2c_start = '1') THEN
-                r_data <= std_logic_vector(std_logic_vector(resize(unsigned(i_device_address), r_data'length)) SLL 1) & i_R_W;
-                r_read_counter <= i_read_byte_nb;
-                r_write_counter <= i_write_byte_nb + 1;
-            ELSE
-                r_data <= r_data;
-            END IF;
-        END IF;
-    END PROCESS p_addressing_routing;
-
     -- State changer and FSM
-    p_state : PROCESS (i_clk, i_rst)
+    p_state : PROCESS (i_clk, i_rst, w_i2c_start, r_read_write_done, r_ack, r_write_counter, r_read_counter)
     BEGIN
         IF (i_rst = '0') THEN
             r_st_present <= ST_IDLE;
@@ -252,7 +244,7 @@ BEGIN
                 IF (r_write_counter = '0') THEN
                     IF (r_read_counter = '0') THEN
                         w_st_next <= ST_END;
-                        w_i2c_end <= '1';
+                        r_i2c_end <= '1';
                     ELSE
                         w_st_next <= ST_READ_DATA;
                     END IF;
@@ -286,14 +278,24 @@ BEGIN
 
             WHEN ST_ERROR =>
                 r_busy <= '1';
-                r_error <= '1';
+                r_error <= '0';
                 r_clock_active <= '0';
-
+                IF (r_error = '1') THEN
+                    w_st_next <= ST_IDLE;
+                ELSE
+                    w_st_next <= r_st_present;
+                END IF;
 
             WHEN ST_END =>
                 r_busy <= '1';
                 r_error <= '0';
                 r_clock_active <= '0';
+                r_i2c_end <= '0';
+                IF (r_i2c_end = '0') THEN
+                    w_st_next <= ST_IDLE;
+                ELSE
+                    w_st_next <= r_st_present;
+                END IF;
 
             WHEN OTHERS =>
                 r_busy <= '0';
